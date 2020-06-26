da1e411039f9a6b5d15bcfaf3d6a784ab69cba66
Pavel <sur-pavel@mail.ru>
2020-06-26 17:26:02 +0300

    sql changes

---
+++ src/resources/create_database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE bugtrackingsystem;
+
+CREATE USER test_user WITH password 'qwerty';
+
+GRANT ALL ON DATABASE bugtrackingsystem TO test_user;
+    
---
+++ src/resources/create_tables.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE projects(
+project_id INTEGER PRIMARY KEY,
+project_name VARCHAR(50),
+file VARCHAR(100)
+);
+
+CREATE TABLE users(
+user_id INTEGER PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+project_id INTEGER,
+FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+CREATE TABLE tasks (
+	task_id INTEGER PRIMARY KEY,
+	project_id INTEGER NOT NULL UNIQUE,
+	user_id INTEGER NOT NULL UNIQUE,
+	theme VARCHAR(50),
+	task_type VARCHAR(50),
+	priority INTEGER,
+	description TEXT
+	FOREIGN KEY (project_id) REFERENCES projects(id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
