5c9e9888de31846be4edfa066efdf5ed256c80fa
Pavel <sur-pavel@mail.ru>
2020-06-26 17:27:17 +0300

    add log and implement create

--- src/main/java/ru/surpavel/bugtrackingsystem/dao/sqlite/UserDaoSQLite.java
+++ src/main/java/ru/surpavel/bugtrackingsystem/dao/sqlite/UserDaoSQLite.java
@@ -2,15 +2,27 @@ package ru.surpavel.bugtrackingsystem.dao.sqlite;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import ru.surpavel.bugtrackingsystem.dao.UserDao;
+import ru.surpavel.bugtrackingsystem.dao.DaoException;
 import ru.surpavel.bugtrackingsystem.domain.User;
 
 public class UserDaoSQLite implements UserDao{
+    private static final Logger log = LogManager.getLogger(UserDaoSQLite.class.getName());
 
     @Override
     public User create(User user) {
-        // TODO Auto-generated method stub
-        return null;
+        log.debug("Creating " + user);
+        try {
+            
+            log.info(user + " was created");
+        } catch (RuntimeException e) {
+            log.error("Can't create " + user, e);
+            throw new DaoException("Can't create " + user, e);
+        }
+        return user;
     }
 
     @Override
