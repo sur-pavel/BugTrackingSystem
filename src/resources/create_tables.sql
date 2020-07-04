f54fe5b6a5ad1dc9ca5ea3cd33f2ad2903851ddb
Pavel <sur-pavel@mail.ru>
2020-07-04 09:30:03 +0300

    changes in relationships of entities

--- src/resources/create_tables.sql
+++ src/resources/create_tables.sql
@@ -6,20 +6,20 @@ file VARCHAR(100)
 );
 
 CREATE TABLE tasks (
-    id INTEGER PRIMARY KEY,
-    project_id INTEGER NOT NULL UNIQUE,    
-    theme VARCHAR(50),
+    id INTEGER PRIMARY KEY,     
+    theme VARCHAR(50) NOT NULL UNIQUE,
     task_type VARCHAR(50),
     priority INTEGER,
     description VARCHAR(100),
+    project_id INTEGER NOT NULL UNIQUE,
     FOREIGN KEY (project_id) REFERENCES projects(id)    
 );
 
 CREATE TABLE users(
 id INTEGER PRIMARY KEY,
-task_id INTEGER NOT NULL UNIQUE,
 first_name VARCHAR(50),
 last_name VARCHAR(50),
+task_id INTEGER NOT NULL UNIQUE,
 FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
