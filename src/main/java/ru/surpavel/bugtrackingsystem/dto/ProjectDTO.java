b64a86414c83164ab8fe3a941a710caa7722a6a3
sur-pavel <sur-pavel@mail.ru>
2021-08-30 18:22:03 +0300

    add id prop

--- src/main/java/ru/surpavel/bugtrackingsystem/dto/ProjectDTO.java
+++ src/main/java/ru/surpavel/bugtrackingsystem/dto/ProjectDTO.java
@@ -1,13 +1,19 @@
 package ru.surpavel.bugtrackingsystem.dto;
 
 public class ProjectDTO {
+    private Long id;
     private String title;
-
+    public Long getId() {
+        return id;
+    }
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
